a2bee135210c4158c2be4b824cdd83c4572c6003
Ryan Papsin <ryanpapsin175@gmail.com>
2022-12-13 17:39:29 -0700

    delete done

--- server.js
+++ server.js
@@ -5,23 +5,17 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const Books = require('./models/Book.js');
+const { response } = require('express');
 const dataBase = mongoose.connection;
 mongoose.connect(process.env.MONGO_URL);
 dataBase.once('open', () => console.log('connected to mongo'));
 
 const PORT = process.env.PORT || 3002;
-
-
-
 const app = express();
+
+
+app.use(express.json());
 app.use(cors());
-
-
-app.get('/test', (request, response) => {
-
-  response.send('test request received')
-
-})
 
 app.get('/books', async (req, res) => {
   let results = [];
@@ -34,6 +28,30 @@ app.get('/books', async (req, res) => {
   }
   res.send(results);
 })
+
+app.post('/books', async (request, respond) => {
+  // request.body;
+
+  console.log(request.body);
+
+  let newBook = await Books.create(request.body);
+
+  respond.send(newBook);
+})
+
+app.delete('/books/:id', async (request, response) => {
+  console.log(request.params._id)
+  try {
+    let id = request.params._id
+    let deletedBook = await books.findByIdAndDelete(id)
+    console.log('here is the value of id: ', id)
+    response.send(deletedBook)
+  } catch (error) {
+    response.send(error);
+  }
+})
+
+
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
 
